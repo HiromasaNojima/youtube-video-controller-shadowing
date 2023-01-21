cebd584f133b1ee989204fb4577fdb9cefce322f
HiromasaNojima <67505578+HiromasaNojima@users.noreply.github.com>
2023-01-21 19:52:31 +0900

    can control play speed

--- controller.js
+++ controller.js
@@ -2,6 +2,9 @@ let player = document.getElementsByTagName("video")[0];
 
 const keySaveCurrentTime = "x";
 const keyGoToSavedTime = "z";
+const keyPlaySpeedUp = "s";
+const keyPlaySpeedDown = "a";
+const keyPlaySpeedDefault = "d";
 let savedTime = null;
 
 document.addEventListener("keydown", function (event) {
@@ -11,6 +14,15 @@ document.addEventListener("keydown", function (event) {
       break;
     case keyGoToSavedTime:
       player.currentTime = savedTime;
+      break;
+    case keyPlaySpeedUp:
+      player.playbackRate += 0.1;
+      break;
+    case keyPlaySpeedDown:
+      player.playbackRate -= 0.1;
+      break;
+    case keyPlaySpeedDefault:
+      player.playbackRate = 1;
       break;
   }
 });
